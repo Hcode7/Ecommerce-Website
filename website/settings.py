bbbac3b94d7f78b1c18a7a1b341ea02bb554c9e7
Hcode7 <amineratit6@gmail.com>
2025-01-07 23:02:38 +0100

    lets work for fucking 100 time

--- website/settings.py
+++ website/settings.py
@@ -82,10 +82,15 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'project',
+        'USER': 'root', 
+        'PASSWORD': 'Amine@123ratit',
+        'HOST': 'localhost',   
+        'PORT': '3306', 
     }
 }
+
 
 
 # Password validation
