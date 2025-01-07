3a5ef177b8d2298974ac1e7c791a8264ff653b0f
Hcode7 <amineratit6@gmail.com>
2025-01-07 21:14:22 +0100

    lets work

--- website/settings.py
+++ website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%tb7u()&ahe)&5o7e772qm!lv-t@sug-o=y7_$-1j-atb1z381'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
