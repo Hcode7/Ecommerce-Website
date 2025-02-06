69607fe41203402e32d5eb72b3cf289dd4221f35
Hcode7 <amineratit6@gmail.com>
2025-02-06 17:12:07 +0100

    solve database problem

--- website/settings.py
+++ website/settings.py
@@ -89,12 +89,8 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'project',
-        'USER': 'root',
-        'PASSWORD': 'Amine@123ratit',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
