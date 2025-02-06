7fdf99f46cabf2249acd3c638b698b29b164a64c
Hcode7 <amineratit6@gmail.com>
2025-02-06 19:11:07 +0100

    connect with postgress db

--- website/settings.py
+++ website/settings.py
@@ -90,12 +90,25 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'KJVPsEyBCmczjzXEpCXAkzBjhepGKffk',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '47898',
     }
 }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
