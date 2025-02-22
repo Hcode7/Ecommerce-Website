1f4a83224832c974a259613a36b6cf5d4ee21152
AmineRatit <mohamedratit@gmail.com>
2025-02-22 09:53:54 +0100

    solve problem of cart

--- app/utils.py
+++ app/utils.py
@@ -2,11 +2,13 @@ from django.core.exceptions import ObjectDoesNotExist
 from .models import CartItem, Cart
 
 def cartitem_counter(request):
-    if not request.user.is_authenticated:
-        return 0
-    
     try:
-        cart = Cart.objects.filter(user=request.user).first()
+        if request.user.is_authenticated:
+            cart = Cart.objects.filter(user=request.user).first()
+        else:
+            cart = Cart.objects.filter(session_key=request.session.session_key).first()
+        if not request.user.is_authenticated:
+            return CartItem.objects.count(cart=cart)
         cart_items = CartItem.objects.filter(cart=cart)
         total_item = sum(item.quantity for item in cart_items)
         return total_item
